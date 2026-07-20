5440bf37a702c7fdb2a0c6fdcee7df023ca6cb8e
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-21 01:29:58 +0530

    Time: 2575 ms (5.00%) | Memory: 86.5 MB (33.02%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         for(int i=1;i<nums.length;i++){
-            int key = nums[i];
-            int j = i-1;
-            while(j>=0 && nums[j] > key){
-            nums[j+1] = nums[j];
+            int key = nums[i];//current element dekho
+            int j = i-1;//prev element dekho
+            while(j>=0 && nums[j] > key){//check karo kya mera prev element mera current key se bada hai if haan then right move kardo woh prev element 
+            nums[j+1] = nums[j];//shift karo element right
             j--;
     }
-    nums[j+1] = key;
+    nums[j+1] = key;// insert karo the key value (saved value)
         }
         return nums;  
     }
