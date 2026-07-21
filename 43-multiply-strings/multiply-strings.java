962f3a58fbb8f09284094ae802f29d2e3b80482b
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-22 02:54:16 +0530

    Time: 3 ms (82.50%) | Memory: 43.6 MB (68.02%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,33 +1,34 @@
 class Solution {
-
     public String multiply(String num1, String num2) {
 
-        // If either number is 0
-        if (num1.equals("0") || num2.equals("0")) {
+        if (num1.equals("0") || num2.equals("0"))
             return "0";
-        }
 
-        int n = num1.length();
-        int m = num2.length();
+        int m = num1.length();
+        int n = num2.length();
 
-        int[] result = new int[n + m];
+        int[] result = new int[m + n];
 
-        // Multiply from right to left
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
 
-            for (int j = m - 1; j >= 0; j--) {
-                int digit1 = num1.charAt(i) - '0';
-                int digit2 = num2.charAt(j) - '0';
-                int product = digit1 * digit2;
-                int sum = product + result[i + j + 1];
-                result[i + j + 1] = sum % 10;
-                result[i + j] += sum / 10;
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
             }
         }
+
         StringBuilder ans = new StringBuilder();
-        for (int num : result) {
-            if (!(ans.length() == 0 && num == 0)) {
-                ans.append(num);
+
+        for (int digit : result) {
+            if (!(ans.length() == 0 && digit == 0)) {
+                ans.append(digit);
             }
         }
 
