67abe9bcd63435058456003484853b5d1ae5e03a
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-23 04:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (81.99%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -5,13 +5,14 @@ class Solution {
         }
         int i=0;
         for(int j=0;j<nums.length;j++)
+        //2 pointers i check last unique element,j scan array if same do nothing and move j else increase i for next unique element
         {
             if(nums[i]!=nums[j]){
                 i++;
                 nums[i]=nums[j];
             }
         }
-        return i+1;//
+        return i+1;//to return index of last unique element
         } 
         
     }
