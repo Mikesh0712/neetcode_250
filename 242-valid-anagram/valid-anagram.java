d14314a4d1d00027f1aae29d8727023342c9bcbb
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-24 04:46:35 +0530

    Time: 4 ms (82.40%) | Memory: 44.3 MB (74.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
+        if(s.length()!=t.length())//check for length if different never valid anagram
         return false;
-        int []a=new int[26];
-        for(int i=0;i<s.length();i++)
+        int []a=new int[26];//frequency store karne ke liye array
+        for(int i=0;i<s.length();i++)//1st string ra elements ra frequency add kariki store kara 
         a[s.charAt(i)-'a']++;
-        for(int i=0;i<t.length();i++)
+        for(int i=0;i<t.length();i++)//2nd string ra elements ra frequency minus kariki from sei puruna freq array ru store kara 
         a[t.charAt(i)-'a']--;
         for(int count:a)
-        {if(count!=0)
+        {if(count!=0)//if array re sabu count 0 thile valid nahale invalid
         return false;}
         return true;
 
