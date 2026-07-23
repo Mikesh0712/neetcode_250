a0ba7f5e5c88b4af616e2ef626b1bf647e0cb9e2
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-24 04:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (55.80%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,9 +2,9 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
         k%=n;//if k>n for eg k=10 n=7 for k=10 and k=3 same array after reversing
-        rev(nums,0,n-1);
-        rev(nums,0,k-1);
-        rev(nums,k,n-1);
+        rev(nums,0,n-1);//reverse complete array
+        rev(nums,0,k-1);//1st k elements reverse kardo
+        rev(nums,k,n-1);//baaki elements from k to last reverse kardo
     }
     private void rev(int nums[],int left,int right){
         while(left<right)
