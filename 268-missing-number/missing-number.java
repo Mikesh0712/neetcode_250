25a45304e8e27cb7cc748289a653254d5b5b44cf
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-24 21:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (66.29%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer>set=new HashSet<>();
-        for(int i=0;i<nums.length;i++)//to put the elements in the set for checking 
-        {
-            set.add(nums[i]);
+    //     HashSet<Integer>set=new HashSet<>();
+    //     for(int i=0;i<nums.length;i++)//to put the elements in the set for checking 
+    //     {
+    //         set.add(nums[i]);
+    //     }
+    //     for(int i=0;i<=nums.length;i++)
+    //     {
+    //         if(!set.contains(i))//checking whether the number is present in set or not 
+    //         return i;
+    //     }
+    //   return -1; 
+
+        int n=nums.length;
+        int total = n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
         }
-        for(int i=0;i<=nums.length;i++)
-        {
-            if(!set.contains(i))//checking whether the number is present in set or not 
-            return i;
-        }
-      return -1; 
+        return total-sum;
+
     }
 }
