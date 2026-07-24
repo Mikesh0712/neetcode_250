b5571fdcf25447daf9f63d7cb0d1842967747574
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-24 23:59:54 +0530

    Time: 1 ms (77.45%) | Memory: 42.8 MB (11.87%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isHappy(int n) {
         HashSet<Integer>set=new HashSet<>();
+        //jebe tak 1 mila
         while(n!=1)
         {
+            //if aau thare milila ta cycle heigala
             if(set.contains(n))
             return false;
-        
+        //number yaad karide
         set.add(n);
         int sum=0;
+        //sabu digit ra square add kar
         while(n>0){
             int digit=n%10;
             sum = sum + digit * digit;
              n = n / 10;
         }
+        //this is the next number which needs to be done till all the digits squares add to zero
         n=sum;}
+        //1 mil gya:)
         return true;
         
     }
