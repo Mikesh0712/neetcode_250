85f81d8fb77bbce2fccb655ba505abe7d2bcf9a9
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-24 23:05:59 +0530

    Time: 37 ms (15.60%) | Memory: 95.8 MB (16.30%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -7,7 +7,7 @@ class Solution {
         }
         int longest=0;
         // Check every number
-        for (int num : set)  {
+        for (int num : set)  {//always do search in set so that duplicate elements are ignored as if done in array duplicate elements will be there causing time limit exceeding
             // Is this the start of a sequence?
             if (!set.contains(num - 1)) {
                 int current = num;
