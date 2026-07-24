4d2802dee21d3e69236ea777485320a0f74cd6d4
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-25 04:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (16.91%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,10 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count=0;
-        int i=s.length()-1;
-        while(s.charAt(i)==' '&&i>=0)
+        int i=s.length()-1;//traversing the string backwards
+        while(s.charAt(i)==' '&&i>=0)//to remove the trailing spaces
         {i--;}
-        while(i>=0&&s.charAt(i)!=' ')
+        while(i>=0&&s.charAt(i)!=' ')//to chcek for characters and increase count until found space and then finish
         {
             count++;
             i--;
