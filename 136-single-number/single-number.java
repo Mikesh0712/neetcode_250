38fca179bac63d965b61d25d8a1791cfbc228c36
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-29 21:22:23 +0530

    Time: 1 ms (99.96%) | Memory: 47 MB (27.98%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
         for(int i=0;i<nums.length;i++){
-            ans^=nums[i];
+            ans^=nums[i];//simple xor to get the non duplicate element
         }
         return ans;
         
