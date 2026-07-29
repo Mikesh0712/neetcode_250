eb1c991aed5e0ed614b3e50efdef4b4355989007
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-29 21:06:30 +0530

    Time: 2 ms (47.22%) | Memory: 43.5 MB (69.86%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,21 +4,21 @@ class Solution {
         int j = b.length() - 1;
         int carry = 0;
         StringBuilder ans = new StringBuilder();
-        // Jab tak dono strings ya carry bacha hai
+        // Jebe tak duita jaka strings ra carry achi 
         while (i >= 0 || j >= 0 || carry != 0) {
             int sum = carry;
-            // a ka current bit add karo
+            // a ra current bit add karo
             if (i >= 0)
                 sum += a.charAt(i--) - '0';
-            // b ka current bit add karo
+            // b ra current bit add karo
             if (j >= 0)
                 sum += b.charAt(j--) - '0';
-            // Current binary digit
+            // append kar current binary digit
             ans.append(sum % 2);
             // Next carry
             carry = sum / 2;
         }
-        // Reverse because humne answer ulta banaya
+        // reverse the answer as our answer is ulta 
         return ans.reverse().toString();
     }
 }
