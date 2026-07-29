db10199b46ea47c42583f8fbb75c48970e35c47d
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-29 22:42:10 +0530

    Time: 9 ms (8.52%) | Memory: 45.7 MB (33.70%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -2,17 +2,21 @@ class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
         int n=nums.length;
-        for(int i=0;i<n-2;i+=3) 
+        //check for groups of 3
+        for(int i=0;i<n-2;i+=3)
         {
+            //jadi 1st and 2nd are not equal not then 1st hi single number 
             if (nums[i]!=nums[i + 1]) 
             {
                 return nums[i];
             }
+            ////jadi 2nd and 3rd are not equal then 2nd number is single number
             if(nums[i+1]!=nums[i+2])
              {
                 return nums[i+2];
             }
         }
+        //if loop re nahi then last element hi single number 
         return nums[n - 1];
     }
 }
