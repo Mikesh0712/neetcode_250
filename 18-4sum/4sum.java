aa76e08cf3635ed2e932b9b3f769801060e00fde
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-07-30 20:00:18 +0530

    Time: 20 ms (65.83%) | Memory: 45.5 MB (95.07%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums,int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            // Duplicate i element skip karo (fix 1st element)
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            //duplicate j element skip karo(fix second element)
+            for(int j=i+1;j<nums.length-2;j++){
+                if(j>i+1&&nums[j]==nums[j-1])
+                continue;
+            
+            int left = j+1;
+            int right = nums.length - 1;
+            while (left < right) {
+                long sum =(long) nums[i] + nums[left] + nums[right]+nums[j];;
+                if (sum == target) {
+                    ans.add(Arrays.asList(nums[i],nums[j], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    // Left duplicates skip karo
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    // Right duplicates skip karo
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+            }
+        }
+        return ans;
+    }
+}
