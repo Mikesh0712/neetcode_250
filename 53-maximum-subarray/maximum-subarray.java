7391d4b60c5d72e9a76354310bfe897f6a51a6a3
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-08-05 19:15:20 +0530

    Time: 1 ms (99.95%) | Memory: 77.1 MB (66.96%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //brute force approach making subarrays for all elements 
+        // int max=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum=sum+nums[j];
+        //         max=Math.max(sum,max);
+        //     }
+        // }
+        // return max;
+
+
+        int sum=0,max=Integer.MIN_VALUE;
+            for(int i=0;i<nums.length;i++){
+                sum+=nums[i];
+                max=Math.max(sum,max);
+                if(sum<0)
+                sum=0;
+            }
+        return max;
+    }
+}
