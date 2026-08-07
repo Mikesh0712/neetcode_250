ac511a6da9b35e5cebb980a2e602a10fc8b42e24
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-08-07 21:07:48 +0530

    Time: 1 ms (17.29%) | Memory: 48.7 MB (70.61%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -5,10 +5,10 @@ class Solution {
 
     int k=1,count=1;
     for(int i=1;i<nums.length;i++){
-        if(nums[i]==nums[i-1])//if same lement then 
+        if(nums[i]==nums[i-1])//if same element then count badhao 
         count++;
-        else count=1;
-        if(count<=2){
+        else count=1;//if nua element then count reset to 1
+        if(count<=2){//maximum 2 thara hi allowed
             nums[k]=nums[i];
             k++;
         }
