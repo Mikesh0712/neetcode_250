5aac9b8d82476de4b07217c9c2342fed8d320b91
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-08-09 21:07:03 +0530

    Time: 6 ms (34.71%) | Memory: 45 MB (51.87%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -5,7 +5,7 @@ class Solution {
         ArrayList<Integer>list=new ArrayList<>();
         int i=0,j=0;
         while(i<nums1.length&&j<nums2.length){
-            if(nums1[i]==nums2[j])
+            if(nums1[i]==nums2[j])//if both elements are same 
             {
                 list.add(nums1[i]);
                 i++;
