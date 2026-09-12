524ae6f42a28326cc327f862a65a3bb0c0c7a0f3
Mikesh0712 <146771502+Mikesh0712@users.noreply.github.com>
2026-09-12 22:00:14 +0530

    Time: 1 ms (98.74%) | Memory: 42.6 MB (71.86%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -2,14 +2,11 @@ class Solution {
     public long countCommas(long n) {
 
         long ans = 0;
-
         long start = 1000;
 
         while (start <= n) {
-
             // Every number from start to n gets one extra comma
             ans += n - start + 1;
-
             start *= 1000;
         }
 
